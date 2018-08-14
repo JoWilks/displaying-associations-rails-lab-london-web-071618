155e446a97625498a79d9081fce5f5474922e95e
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-14 16:01:38 +0100

    Jo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+adele = Artist.create(name: "Adele")
+hello = Song.create(title: "Hello")
+
+adele.songs << hello
